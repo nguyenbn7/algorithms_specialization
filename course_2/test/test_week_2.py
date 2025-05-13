6f3e5812f6ecadd653494d51a3f198952c388040
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-05-13 08:24:27 +0700

    update test week 2 course 2

--- course_2/test/test_week_2.py
+++ course_2/test/test_week_2.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 import test as _
 
-from src.week_1 import find_scc
+from src.week_2 import find_shortest_paths, GraphNode
 
 current_path = Path(__file__).parent.resolve()
 
@@ -15,24 +15,21 @@ output_test_filenames = [f.name for f in output_test_path.glob("*.txt")]
 
 
 def get_graph(filename: str):
+    graph = {}
+
     with open(
         input_test_path / filename,
         "r",
     ) as f:
-        lines = f.readlines()
+        for line in f:
+            temp = line.split()
+            v, edges = int(temp[0]), temp[1:]
+            graph[v] = []
+            for e in edges:
+                ve, w = map(int, e.split(","))
+                graph[v].append(GraphNode(ve, w))
 
-        data = [tuple(map(lambda x: int(x), l.split())) for l in lines]
-
-        len_graph = max(max(data)) + 1
-
-        graph = [[] for _ in range(len_graph)]
-        rev_graph = [[] for _ in range(len_graph)]
-
-        for v, e in data:
-            graph[v].append(e)
-            rev_graph[e].append(v)
-
-    return (graph, rev_graph, len_graph)
+        return graph
 
 
 def get_expected_test_result(filename):
@@ -47,15 +44,13 @@ def get_expected_test_result(filename):
     "input_filename,output_filename",
     [*zip(input_test_filenames, output_test_filenames)],
 )
-def test_scc(input_filename, output_filename):
+def test_graph_shortest_paths(input_filename, output_filename):
     expected = get_expected_test_result(output_filename)
 
-    (graph, rev_graph, len_graph) = get_graph(input_filename)
+    graph = get_graph(input_filename)
 
-    scc = find_scc(graph, rev_graph, len_graph)
+    shortest_paths = find_shortest_paths(graph)
 
-    scc.sort(reverse=True)
-
-    actual = scc[:5]
+    actual = [shortest_paths[i] for i in [7, 37, 59, 82, 99, 115, 133, 165, 188, 197]]
 
     assert actual == expected
